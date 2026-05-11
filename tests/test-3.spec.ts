c95b34668678bfab90def5b0174c6fbdd5af06dc
Asiel <asiel.alabbassa>
2026-05-10 23:02:59 -0400

    test should fail and see how the github reporter helps to understand the reason of the fail

--- tests/test-3.spec.ts
+++ tests/test-3.spec.ts
@@ -18,7 +18,7 @@ test('test', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Enter tags' }).fill('tag entered');
   await page.getByRole('textbox', { name: 'Enter tags' }).press('Enter');
   await page.getByRole('button', { name: 'Publish Article' }).click();
- await expect(page.getByRole('heading')).toHaveText('Asiel article');
+ await expect(page.getByRole('heading')).toHaveText('Asiel article xxxx');
  await expect(page.locator('.tag-list li')).toHaveText('tag entered');
  await page.getByRole('button', { name: 'Delete Article' }).nth(1).click();
   
